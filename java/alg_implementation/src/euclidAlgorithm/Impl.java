ef1ad3a26b4c05bc575f8a7a195c41edc64ba3cb
Khushdeep Singh <dream.chamcore@gmail.com>
2020-07-28 18:42:44 +0530

    algorithm implementation

--- java/alg_implementation/src/euclidAlgorithm/Impl.java
+++ java/alg_implementation/src/euclidAlgorithm/Impl.java
@@ -1,4 +1,24 @@
 package euclidAlgorithm;
 
+import java.util.Scanner;
+
 public class Impl {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter the first value: ");
+        int value1 = input.nextInt();
+        System.out.println("Enter the second value: ");
+        int value2 = input.nextInt();
+        int remainder;
+        do {
+            remainder = value1 % value2;
+            if (remainder !=0){
+                value1 = value2;
+                value2 = remainder;
+            }
+
+
+        }while (remainder !=0);
+        System.out.println("GCD is :"+value2);
+    }
 }
